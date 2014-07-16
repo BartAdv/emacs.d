19cae4375a5ceb5f727ae0748b2c6f4340084a86
BartAdv <bprzygoda@gmail.com>
2014-07-16 17:30:09 +0000

    erlang tools version bump

--- erlang.el
+++ erlang.el
@@ -1,9 +1,12 @@
-(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.14/emacs" load-path))
-(setq erlang-root-dir "/usr/local/lib/erlang")
-(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+(setq load-path (cons "/usr/lib/erlang/lib/tools-2.6.15/emacs" load-path))
+(setq erlang-root-dir "/usr/lib/erlang")
+(setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
 (require 'erlang-start)
 
 (substitute-unicode 'erlang-mode
 		    (list (cons "\\b\\(fun\\)\\b" 'lambda)))
+
+(add-to-list 'load-path "~/emacs/edts")
+(require 'edts-start)
 
 (provide 'erlang)
